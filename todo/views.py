b7277079698b21a25f6d33699d27ce8c6c70b0f9
Nitya Shukla <nityashukla@Nityas-MacBook-Pro.local>
2025-11-11 15:30:52 +0530

    add task functionalies

---
+++ todo/views.py
@@ -0,0 +1,8 @@
+from .models import Task
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
--- todo_main/urls.py
+++ todo_main/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name='home'),
+    
+    #TO DO
+    path('todo/', include('todo.urls')),
 ]
